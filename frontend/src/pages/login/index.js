e150bcd9edbfe4b1a22ab54573b07ffbf6254ab1
rasiya <rasiyajishar1@gmail.com>
2024-01-03 11:57:57 +0530

    redux added,store and userReducer created,login page design

---
+++ frontend/src/pages/login/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='login'>
+<div className='login_wrapper'>
+<div className='login_wrap'>
+<div className='login1'>SocialHub
+<span>We don't just share posts, we share experiences.
+Engaging more, Refreshing result</span>
+
+</div>
+<div className='login2'></div>
+<div className='login2_wrap'>
+<form></form>
+
+
+</div>
+</div>
+<div className='register'></div>
+</div>
+</div>
+  )
+}
+
+export default Login
